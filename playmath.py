4eaf7166456c41f9812aa7c791918e3cc87246cf
Mehul Chopra <Mehul.Chopra@avalara.com>
2018-10-12 19:38:31 +0530

    math, stats, start with rest

---
+++ playmath.py
@@ -0,0 +1,12 @@
+import math
+
+print(math.ceil(3.4))
+print(math.floor(3.4))
+
+print(math.factorial(6))
+print(math.gcd(32, 16))
+
+print(math.isnan(45))
+print(math.sqrt(16))
+
+print(math.pi)
---
+++ playstats.py
@@ -0,0 +1,9 @@
+import statistics
+
+marks = [5, 6, 2, 3, 10, 9, 10, 10, 7]
+print(statistics.mean(marks))
+print(statistics.median(marks))
+print(statistics.mode(marks))
+
+print(statistics.stdev(marks))
+print(statistics.variance(marks))
